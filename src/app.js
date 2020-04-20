4d754ede2b2b38466e334e7abb5eb02fbb4432ec
Craig Barratt <craigtbarratt@gmail.com>
2020-04-20 19:08:19 +0100

    MVP

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-new Vue({
-  el: "#app",
-  data: [],
-  methods: {},
-});
+  new Vue({
+    el: "#app",
+    data: {
+      todos: [],
+      newToDo: "",
+      priority: ""
+    },
+    methods: {
+      saveNewToDo: function() {
+        const toDoToAdd = {
+          name: this.newToDo,
+          priority: this.priority
+        }
+        this.todos.push(toDoToAdd);
+        this.newToDo = ""
+      }
+    },
+  });
 
 });
